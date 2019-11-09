f1fb405da75e67ab9c76134186bcd94495b3aa23
bagya-hennayake <bagyasrnat@gmail.com>
2019-11-10 02:36:34 +0530

    tab controller done

---
+++ NIBM-App/Controllers/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  NIBM-App
+//
+//  Created by Bagya Hennayake on 11/10/19.
+//  Copyright © 2019 Bagya Hennayake. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
